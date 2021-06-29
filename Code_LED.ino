d2d2e76fafeaacf1e9ef952e217a9664d350bdc7
AbdurrahmanSardar <86242764+AbdurrahmanSardar@users.noreply.github.com>
2021-06-29 19:15:16 +0300

    Update Code_LED.ino

--- Code_LED.ino
+++ Code_LED.ino
@@ -2,13 +2,12 @@
 //
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);          // set pin13 as output
 }
 
-void loop()
-{
-  digitalWrite(13, HIGH);
-  delay(10); // Wait for 10 millisecond(s)
-  digitalWrite(13, LOW);
-  delay(10); // Wait for 10 millisecond(s)
+void loop(){
+  digitalWrite(13, HIGH);      // set the switch on
+  delay(10);                   // Wait for 10 millisecond(s)
+  digitalWrite(13, LOW);       // set the switch off
+  delay(10);                   // Wait for 10 millisecond(s) 
 }
